b951b1a5f4efd35673e0270ec8c9f50efeb0c17d
sandipanbasu <sandipan@whynotcode.in>
2020-07-22 07:28:20 +0000

    update docker and generated requirements

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# create build folder
+echo "delete build dir"
+rm -rf build
+mkdir build
+cp docker-compose.yml build/
+cp -r flask_app build/
+cp -r nginx build/
+cp -r src build/flask_app/src
+
+cd build
+
+
 echo "killing old docker processes"
 docker-compose rm -fs
 
